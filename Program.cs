fb914afd8aac383e2d4b6e1e6008b7524d621486
F3700 <albertfebrian26@gmail.com>
2025-03-09 18:42:51 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class KodePos {
+    public enum kelurahan {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+    private int[] kodepos = {40266,
+        40287,
+        40267,
+        40256,
+        40287,
+        40286,
+        40286,
+        40286,
+        40272,
+        40274,
+        40273
+    };
+
+    public int getKodePost(kelurahan x) {
+        return this.kodepos[(int)x];
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        KodePos kodePos = new KodePos();
+
+    }
+}
